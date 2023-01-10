f9affdb0ffe407ff1066b35c9afc8ff18c56c8e6
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-10 16:58:38 +0530

    small program in angular

--- even-odd-app/src/app/app-routing.module.ts
+++ even-odd-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArrayComponent } from './pages/array/array.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component : ArrayComponent,
+    path : "array"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
