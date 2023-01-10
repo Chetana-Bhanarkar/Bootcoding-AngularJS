7d738559df27312c838ef94227d883712dd81f2e
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-10 14:56:43 +0530

    small project on angular

--- even-odd-app/src/app/pages/array/array.component.ts
+++ even-odd-app/src/app/pages/array/array.component.ts
@@ -5,16 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './array.component.html',
   styleUrls: ['./array.component.css']
 })
+
 export class ArrayComponent {
   title = "Performance of array";
   note  = "Enter the comma separated value -"
   elements!: string;
   result = "";
+  array :Number[]= [];
 
   constructor() {
   }
 
-  minArray() {
+  minArray(): void {
+    this.result = ""
+    this.array= []
     let nums = this.elements.split(',').map(Number); // array of numbers
     let min = nums[0];
     for (let i = 1; i < nums.length; i++) {
@@ -26,6 +30,8 @@ export class ArrayComponent {
   }
 
   maxArray(){
+    this.result = ""
+    this.array= []
     let nums = this.elements.split(",").map(Number);
     let max = nums[0];
     for(let i = 1 ; i<nums.length;i++){
@@ -36,44 +42,52 @@ export class ArrayComponent {
     this.result = "max element is " + max;
   }
 
-  array :Number[]= [];
   // odd: boolean = false;
   reverseArray(){
+    this.array = []
+    this.result = ""
     let nums = this.elements.split(",").map(Number);
-    for(let i=nums.length;i>=0;i--){
+    for(let i=nums.length-1;i>=0;i--){
       this.array.push(nums[i]);
     }
-    this.result = "reverse array is  "+this.array;
+    // this.result = "reverse array is  "+this.array;
   }
 
   sizeOfArray(){
+    this.result = ""
+    this.array= []
     let nums = this.elements.split(',').map(Number);
     let size = nums.length;
     this.result = "The length of above array is " + size
   }
 
   oddArrayElement(){
+    this.array = []
+    this.result = ""
     let nums = this.elements.split(',').map(Number);
     for(let i=0;i<nums.length;i++){
       if(nums[i]%2 != 0){
         this.array.push(nums[i]);
       }
     }
-    this.result = "Odd element is "+ this.array;
+    // this.result = "Odd element is "+ this.array;
     // this.odd = true;
   }
   evenArrayElement(){
+    this.array= []
+    this.result = ""
     let nums = this.elements.split(',').map(Number);
     for(let i=0;i<nums.length;i++){
       if(nums[i]%2 == 0){
         this.array.push(nums[i]);
       }
     }
-    this.result = "Even element is "+ this.array;
+    // this.result = "Even element is "+ this.array;
   }
 
   clear(){
-    this.elements = "";
+    this.array = [];
+    this.elements=""
     this.result = " ";
   }
 }
