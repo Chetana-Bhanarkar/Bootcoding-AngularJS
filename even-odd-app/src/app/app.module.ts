5d4a3252dff28bf7b4351ad0f5b5aa1ad4c3f931
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-18 10:39:45 +0530

    angular project

--- even-odd-app/src/app/app.module.ts
+++ even-odd-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ArrayComponent } from './pages/array/array.component';
 import { NumberComponent } from './pages/number/number.component';
+import { AdditionComponent } from './addition/addition.component';
 // import { CheckArrayComponent } from './pages/check-array/check-array.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { NumberComponent } from './pages/number/number.component';
     AppComponent,
     ArrayComponent,
     NumberComponent,
+    AdditionComponent,
     // CheckArrayComponent
   ],
   imports: [
