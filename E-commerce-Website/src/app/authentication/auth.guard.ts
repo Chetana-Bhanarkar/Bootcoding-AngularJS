4bb4269c7b6dc8955b373f0756af27eab04c5770
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-04-21 11:10:17 +0530

    auth guard ts file

---
+++ E-commerce-Website/src/app/authentication/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CanActivate } from '../../../src/app/authGuard_interface'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return false;
+  }
+
+
+
+}
+
+
