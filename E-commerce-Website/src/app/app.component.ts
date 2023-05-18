7d072cf114463c860eae6c89814ed90762a3f35d
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-18 10:59:30 +0530

    app component

--- E-commerce-Website/src/app/app.component.ts
+++ E-commerce-Website/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SellerService } from './service/seller.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private seller : SellerService){}
   title = 'E-commerce-Website';
+
+  ngOnInit():void{
+    this.seller.reloadSeller() ; 
+  }
 }
