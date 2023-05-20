8ffd9b6768f8c3cfa6a419fccc191964f0b7c91b
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-20 14:40:04 +0530

    seller auth ts file

--- E-commerce-Website/src/app/pages/seller-auth/seller-auth.component.ts
+++ E-commerce-Website/src/app/pages/seller-auth/seller-auth.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { signup } from 'src/model/sellerForm.model';
+import { SellerService } from 'src/app/service/seller.service';
+
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+  static checkLogin() {
+    throw new Error("Method not implemented.");
+  }
 
-  SignUp(data:object){
-    console.warn(data);
 
+  constructor(private seller: SellerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.seller.reloadSeller();
+  }
+
+  SignUp(data: signup) : void {
+    console.log(data);
+    this.seller.userSignUp(data);
+    // this.router.navigate(['seller-home']);
   }
 }
