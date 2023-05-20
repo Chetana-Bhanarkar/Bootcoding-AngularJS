2421286c50630b44f82786dc50f8eb013ae8fb75
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-05-20 14:39:07 +0530

    app module

--- E-commerce-Website/src/app/app.module.ts
+++ E-commerce-Website/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SellerAuthComponent } from './pages/seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SellerHomeComponent } from './pages/seller-home/seller-home.component';
 
 @NgModule({
   declarations: [
@@ -13,13 +16,19 @@ import { SellerAuthComponent } from './pages/seller-auth/seller-auth.component';
     HeaderComponent,
     HomeComponent,
     SellerAuthComponent,
+    SellerHomeComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
